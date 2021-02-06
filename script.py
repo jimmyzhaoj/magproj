b78042f69815eba5cddcd2f2f2e2500926f62b6f
Jimmy Zhao <zhao.jimmy@hotmail.com>
2021-02-06 15:11:10 -0500

    remmoved some code.

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
-import math
-import sys
-from os import rename
+# import math
+# import sys
+# from os import rename
 
 import requests
 
-name = input("Your name? ")
-print("Hello,", name)
+# name = input("Your name? ")
+# print("Hello,", name)
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
+# def greet(who_to_greet):
 
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 # print(greet('Would'))
 # print(greet("Jimmy"))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
